e7d291e9dd95c6db47bf9424f718c84cb1118baa
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-27 14:27:00 +0400

    7/27/24

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.set('view engine','ejs');
 app.get("/", function(req, res){
     res.render("index");
 });
+app.get('/profile/:username', function(req,res){
+    res.send(req.params.username);
+})
+app.get('/profile/:username/:age', function(req,res){
+    res.send(req.params.username);
+})
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
